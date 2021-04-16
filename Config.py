abded6bce980e9d5dfa29de6ab5a2acc32458080
chengkangzai <pycck@hotmail.com>
2021-04-16 12:45:07 +0800

    fix typo

--- Config.py
+++ Config.py
@@ -4,9 +4,9 @@ import os
 
 class Config:
     def __init__(self):
-        env = dotenv_values(".env")
+        env = dotenv_values(".env.local")
         self.APP_DEBUG = os.environ.get("APP_DEBUG", env.get("APP_DEBUG", True))
-        self.APP_DEBUG = os.environ.get("APP_ENV", env.get("APP_ENV", "production"))
+        self.APP_ENV = os.environ.get("APP_ENV", env.get("APP_ENV", "production"))
 
         self.DB_HOST = os.environ.get("DB_HOST", env.get('DB_HOST', '127.0.0.1'))
         self.DB_PORT = os.environ.get("DB_PORT", env.get('DB_PORT', '3306'))
@@ -16,3 +16,20 @@ class Config:
 
         self.DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN", env.get("DISCORD_TOKEN"))
         self.COMMAND_PREFIX = os.environ.get("COMMAND_PREFIX", env.get("COMMAND_PREFIX"))
+
+
+if __name__ == "__main__":
+    print('1. Print All the Config')
+    operation = input("What you want")
+    if operation == "1":
+        print(f"""
+APP_DEBUG       = {Config().APP_DEBUG} 
+APP_ENV         = {Config().APP_ENV} 
+DB_HOST         = {Config().DB_HOST} 
+DB_PORT         = {Config().DB_PORT} 
+DB_DATABASE     = {Config().DB_DATABASE} 
+DB_USERNAME     = {Config().DB_USERNAME} 
+DB_PASSWORD     = {Config().DB_PASSWORD} 
+DISCORD_TOKEN   = {Config().DISCORD_TOKEN} 
+COMMAND_PREFIX  = {Config().COMMAND_PREFIX} 
+       """)
